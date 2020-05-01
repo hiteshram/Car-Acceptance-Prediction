68a762adb3025b69b3f797fc7012181668eeaa55
rags7 <62184181+rags7@users.noreply.github.com>
2020-04-30 19:34:13 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,48 @@
 class Assigned:
-    def __init__(self, car_dataset_filename):
-        self.car_dataset = car_dataset_filename
+    def __init__(self, car_full_data,car_categorical_data):
+        self.car_full_data = car_full_data
+        self.car_categorical_data = car_categorical_data
         #self.judge_filename = judge_filename
 
         import pandas as pd
         # First load the dataset into pandas dataframe
-        self.car_dataset = pd.read_csv(car_dataset_filename,delimiter=',')
-        #self.judge_dataset = pd.read_csv(judge_filename,delimiter=',')
+        self.car_full_data = pd.read_csv(car_full_data,delimiter=',')
+        self.car_categorical_data = pd.read_csv(car_categorical_data,delimiter = ',', header = None)
 
 
 
 
     #Imported methods
     from .tasks.datatranslation import datatranslation
+    from .tasks.convert_car_data import convert_car_data
+    # from .tasks.knn import knn
+    # from .tasks.Naive_Bayern import Naive_Bayern
+    # from .tasks.SVM import svm
+    # from .tasks.Decision_Tree import decision_tree
+    # from .tasks.classifiers import classifiers
+    from .tasks.input_ import input_
+    from .tasks.prediction import prediction
+    from .tasks.Detection import detection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
