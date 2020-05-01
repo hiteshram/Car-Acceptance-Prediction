311990531db645aebe32229a76e703b68c539b81
rags7 <62184181+rags7@users.noreply.github.com>
2020-04-30 19:32:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from Car import *
 
-q = Assigned(car_dataset_filename='./dataset/car.csv')
-             #judge_filename='dataset/judge-without-labels.csv')
+q = Assigned(car_full_data ='./dataset/car.csv',
+             car_categorical_data = './dataset/car_data.csv')
 
-data = q.car_dataset
-print(q.datatranslation(data))
+
+
+data = q.car_full_data
+d = q.car_categorical_data
+integer_data = q.convert_car_data(d)
+print(q.prediction(data,integer_data))
+
+
+
