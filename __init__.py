3b79ac26ae84443d4d375badc7442599f6ec1638
Raghu Veera Reddy Malreddy <62184181+rags7@users.noreply.github.com>
2020-05-12 19:06:13 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ class Assigned:
     # from .tasks.Naive_Bayern import Naive_Bayern
     # from .tasks.SVM import svm
     # from .tasks.Decision_Tree import decision_tree
-    # from .tasks.classifiers import classifiers
+    from .tasks.classifiers import classifiers
     from .tasks.input_ import input_
     from .tasks.prediction import prediction
     from .tasks.Detection import detection
