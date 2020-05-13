1756f4d043a194d5d9fc8bad9ffd02168c7e2c24
Raghu Veera Reddy Malreddy <62184181+rags7@users.noreply.github.com>
2020-05-13 12:31:56 -0600

    Update convert_car_data.py

--- Car/tasks/convert_car_data.py
+++ Car/tasks/convert_car_data.py
@@ -1,14 +1,18 @@
 def convert_car_data(self, car_full_data):
+    
+    ##Encoding the car data from categorical to integer ##
     import pandas as pd
     pd.options.display.max_columns = None
     pd.options.display.width = None
     pd.options.display.max_rows = None
 
     gdata = car_full_data
-    gdata = pd.DataFrame(gdata)
+    gdata = pd.DataFrame(gdata) ##Converting into Dataframe to make easy for encoding ##
+    ##Renaming the columns with the columns names provided in names list for the dataset ##
     gdata.rename(columns={0: 'buying', 1: 'maint', 2: 'doors', 3: 'persons', 4: 'lug_boot', 5: 'safety', 6: 'prediction'}, inplace=True)
-
-    gdata['buying'],_ = pd.factorize(gdata['buying'])
+    
+     ##Using pandas factorize encoding the values here down ##
+    gdata['buying'],_ = pd.factorize(gdata['buying']) 
     gdata['maint'],_ = pd.factorize(gdata['maint'])
     gdata['doors'],_ = pd.factorize(gdata['doors'])
     gdata['persons'],_ = pd.factorize(gdata['persons'])
